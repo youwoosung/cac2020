6ab2f3a5abb0ae94a020d760ec69eda4b5db674c
youwoosung <67929857+youwoosung@users.noreply.github.com>
2020-07-25 10:35:42 -0400

    Update ArrayTest.java

--- ArrayTest.java
+++ ArrayTest.java
@@ -1,14 +1,12 @@
 public class ArrayTest {
 	public static void main(String[] args) {
 			
-		//ingredients
-		String[] spaghetti = {"Pasta noodles", "Tomatoes", "Tomato sauce", 
-				"Cheese", "Garlic", "Basil", 
-				"Onion", "Ground beef", "Black pepper", "Olive oil"};
-		
-		String[] alfredo = {"Pasta noodles", "Butter", "Heavy cream", 
-				"Garlic", "Parmesan cheese"};
-		
+		// ingredients
+		String[] spaghetti = { "Pasta noodles", "Tomatoes", "Tomato sauce", "Cheese", "Garlic", "Basil", "Onion",
+				"Ground beef", "Black pepper", "Olive oil" };
+
+		String[] alfredo = { "Pasta noodles", "Butter", "Heavy cream", "Garlic", "Parmesan cheese" };
+
 		String[][] dishes = new String[2][10];
 		dishes[0] = spaghetti;
 		dishes[1] = alfredo;
@@ -19,6 +17,28 @@ public class ArrayTest {
 			}
 			System.out.println("");
 		}
+		
+		
+		
+		String[] keywords = {"Pasta noodles", "Garlic", "Basil", "Butter", "Tomatoes"};
+		
+		int count = 0;
+		float percentage = 0;
+		
+		for (int i = 0; i < spaghetti.length; i++) {
+			for (int a = 0; a < keywords.length; a++) {
+				if (spaghetti[i].equals(keywords[a])) {
+					count = count + 1;
+				}
+			}
+		}
+		
+		percentage = ((float)count/(float)spaghetti.length)*100;
+		
+		System.out.println(count + ", " + percentage);
+		
+		
+		
 		
 		
 		
