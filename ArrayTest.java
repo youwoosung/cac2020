5718826a724ab17b9fb45ce1a5123188b454d634
youwoosung <67929857+youwoosung@users.noreply.github.com>
2020-07-25 11:40:58 -0400

    Update ArrayTest.java

--- ArrayTest.java
+++ ArrayTest.java
@@ -2,10 +2,11 @@ public class ArrayTest {
 	public static void main(String[] args) {
 
 		// ingredients
-		String[] spaghetti = { "Pasta noodles", "Tomatoes", "Tomato sauce", "Cheese", "Garlic", "Basil", "Onion",
-				"Ground beef", "Black pepper", "Olive oil" };
-
-		String[] alfredo = { "Pasta noodles", "Butter", "Heavy cream", "Garlic", "Parmesan cheese" };
+		String[] spaghetti = {"Spaghetti", "Pasta noodles", "Tomatoes", "Tomato sauce", "Cheese", "Garlic", "Basil", "Onion", "Ground beef", "Black pepper", "Olive oil"};
+		String[] alfredo = {"Alfredo", "Pasta noodles", "Butter", "Heavy cream", "Garlic", "Parmesan cheese"};
+		String[] friedRice = {"Fried Rice", "Egg", "Garlic", "Rice", "Scallion", "Onion", "Carrot", "Soy sauce", "Peas", "Sesame oil"};
+		String[] cheeseSandwich = {"Cheese Sandwich", "Cheese", "Bread", "Butter"};
+		String[] omelette = {"Omelette", "Egg", "Cheese", "Butter", "Onion", "Black pepper", "Ham", "Milk"};
 
 		String[][] dishes = new String[2][10];
 		dishes[0] = spaghetti;
@@ -20,7 +21,7 @@ public class ArrayTest {
 		
 		String[] keywords = {"Pasta noodles", "Garlic", "Basil", "Butter", "Tomatoes"};
 		
-		float percentage = caculatePercentage(spaghetti, keywords);
+		caculatePercentage(spaghetti, keywords);
 	}
 	
 	private static float caculatePercentage(String[] dish, String[] keywords) {
