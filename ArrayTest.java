4c1e7c84d76763153d549b8d53de2325b4d8f79e
youwoosung <67929857+youwoosung@users.noreply.github.com>
2020-07-25 10:43:38 -0400

    Update ArrayTest.java

--- ArrayTest.java
+++ ArrayTest.java
@@ -1,6 +1,6 @@
 public class ArrayTest {
 	public static void main(String[] args) {
-			
+
 		// ingredients
 		String[] spaghetti = { "Pasta noodles", "Tomatoes", "Tomato sauce", "Cheese", "Garlic", "Basil", "Onion",
 				"Ground beef", "Black pepper", "Olive oil" };
@@ -18,64 +18,27 @@ public class ArrayTest {
 			System.out.println("");
 		}
 		
-		
-		
 		String[] keywords = {"Pasta noodles", "Garlic", "Basil", "Butter", "Tomatoes"};
 		
+		float percentage = caculatePercentage(spaghetti, keywords);
+	}
+	
+	private static float caculatePercentage(String[] dish, String[] keywords) {
 		int count = 0;
 		float percentage = 0;
 		
-		for (int i = 0; i < spaghetti.length; i++) {
+		for (int i = 0; i < dish.length; i++) {
 			for (int a = 0; a < keywords.length; a++) {
-				if (spaghetti[i].equals(keywords[a])) {
+				if (dish[i].equals(keywords[a])) {
 					count = count + 1;
 				}
 			}
 		}
 		
-		percentage = ((float)count/(float)spaghetti.length)*100;
+		percentage = ((float)count/(float)dish.length)*100;
 		
 		System.out.println(count + ", " + percentage);
 		
-		
-		
-		
-		
-		
-//		//what you have in refrigerator
-//		String[] keywords = {"Pasta noodles", "Garlic", "Basil", "Butter", "Tomatoes"};
-		
-//		int count = 0;
-//		float percentage = 0;
-//		
-//		for (int i = 0; i < spaghetti.length; i++) {
-//			for (int a = 0; a < keywords.length; a++) {
-//				if (spaghetti[i].equals(keywords[a])) {
-//					count = count + 1;
-//				}
-//			}
-//		}
-//		
-//		percentage = ((float)count/(float)spaghetti.length)*100;
-//		
-//		System.out.println(count + ", " + percentage);
-//		
-//		count = 0;
-//		
-//		for (int i = 0; i < alfredo.length; i++) {
-//			for (int a = 0; a < keywords.length; a++) {
-//				if (alfredo[i].equals(keywords[a])) {
-//					count = count + 1;
-//					percentage = (count/spaghetti.length)*100;
-//				}
-//			}
-//		}
-//		
-//		percentage = ((float)count/(float)spaghetti.length)*100;
-//		
-//		System.out.println(count + ", " + percentage);
-
-		
-		
+		return percentage;
 	}
 }
