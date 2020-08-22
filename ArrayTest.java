f7a242ed5a8783e8107375000bc83e7ffbb55535
emster218 <67984463+emster218@users.noreply.github.com>
2020-08-22 10:29:54 -0400

    Update ArrayTest.java

--- ArrayTest.java
+++ ArrayTest.java
@@ -2,17 +2,17 @@ public class ArrayTest {
 	public static void main(String[] args) {
 
 		// ingredients
-		String[] spaghetti = { "Spaghetti", "Pasta noodles", "Tomatoes", "Tomato sauce", "Cheese", "Garlic", "Basil",
+		String[] spaghetti = { "Spaghetti", "", "Pasta noodles", "Tomatoes", "Tomato sauce", "Cheese", "Garlic", "Basil",
 				"Onion", "Ground beef", "Black pepper", "Olive oil" };
 
-		String[] alfredo = { "Alfredo", "Pasta noodles", "Butter", "Heavy cream", "Garlic", "Parmesan cheese" };
+		String[] alfredo = { "Alfredo", "", "Pasta noodles", "Butter", "Heavy cream", "Garlic", "Parmesan cheese" };
 
-		String[] friedrice = { "Fried Rice", "Egg", "Garlic", "Rice", "Scallion", "Onion", "Carrot", "Soy sauce",
+		String[] friedrice = { "Fried Rice", "", "Egg", "Garlic", "Rice", "Scallion", "Onion", "Carrot", "Soy sauce",
 				"Peas", "Sesame oil" };
 
-		String[] cheesesandwich = { "Cheese Sandwich", "Cheese", "Bread", "Butter" };
+		String[] cheesesandwich = { "Cheese Sandwich", "", "Cheese", "Bread", "Butter" };
 
-		String[] omelette = { "Omelette", "Cheese", "Butter", "Onion", "Black Pepper", "Ham", "Milk" };
+		String[] omelette = { "Omelette", "", "Cheese", "Butter", "Onion", "Black Pepper", "Ham", "Milk" };
 
 		String[][] dishes = new String[5][10];
 		dishes[0] = spaghetti;
@@ -21,26 +21,25 @@ public class ArrayTest {
 		dishes[3] = cheesesandwich;
 		dishes[4] = omelette;
 
-		printArray(spaghetti);
-		printArray(alfredo);
-		printArray(friedrice);
-		printArray(cheesesandwich);
-		printArray(omelette);
-
+		
 		String[] keywords = { "Pasta noodles", "Garlic", "Basil", "Butter", "Tomatoes" };
-
-		float percentage = caculatePercentage(spaghetti, keywords);
-		percentage = caculatePercentage(alfredo, keywords);
-		percentage = caculatePercentage(friedrice, keywords);
-		percentage = caculatePercentage(cheesesandwich, keywords);
-		percentage = caculatePercentage(omelette, keywords);
+		
+		for (int i = 0; i < dishes.length; i++) {
+			float percentage = caculatePercentage(dishes[i], keywords);
+			dishes[i][1] = String.valueOf(percentage);
+		}
+		
+		for (int i = 0; i < dishes.length; i++) {
+			printArray(dishes[i]);
+		}
 	}
+	
 
 	private static float caculatePercentage(String[] dishes, String[] keywords) {
 		int count = 0;
 		float percentage = 0;
 
-		for (int i = 0; i < dishes.length; i++) {
+		for (int i = 2; i < dishes.length; i++) {
 			for (int a = 0; a < keywords.length; a++) {
 				if (dishes[i].equals(keywords[a])) {
 					count = count + 1;
@@ -48,20 +47,30 @@ public class ArrayTest {
 			}
 		}
 
-		percentage = ((float) count / ((float) dishes.length - 1)) * 100;
+		percentage = ((float) count / ((float) dishes.length - 2)) * 100;
 
-		System.out.println(dishes[0] + ": " + percentage + "%");
+//		System.out.println(dishes[0] + ": " + percentage + "%" + " - " + dishes[1] + ", " + dishes[2] + ", " + dishes[3]);
 
 		return percentage;
 	}
 
+//	private static void printArray(String[] dishes) {		
+//		System.out.print(dishes[0] + ": ");
+//		for (int i = 1; i < dishes.length; i++) {
+//			System.out.print(dishes[i] + ",");
+//		}
+//
+//		System.out.println("");
+//	}
+
+
 	private static void printArray(String[] dishes) {
 		System.out.print(dishes[0] + ": ");
-		for (int i = 1; i < dishes.length; i++) {
-			System.out.print(dishes[i] + ",");
+		System.out.print(dishes[1] + "%, ");
+		for (int i = 2; i < dishes.length; i++) {
+		System.out.print(dishes[i] + ", ");
 		}
 
 		System.out.println("");
 	}
-
 }
