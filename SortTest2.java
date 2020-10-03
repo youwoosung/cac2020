b240c85909f58bf550615f1609afac17cfcf8869
youwoosung <67929857+youwoosung@users.noreply.github.com>
2020-10-03 09:58:12 -0400

    Update SortTest2.java

--- SortTest2.java
+++ SortTest2.java
@@ -1,50 +1,77 @@
 public class SortTest2 {
 
 	public static void main(String[] args) {
-		String[] spaghetti = { "Spaghetti", "100.0" };
-		String[] alfredo = { "Alfredo", "12.23" };
-		String[] friedrice = { "Fried Rice", "50.0" };
-		String[] cheesesandwich = { "Cheese Sandwich", "2.4" };
-		String[] omelette = { "Omelette", "99.9" };
-
-		String[][] dishes = new String[5][10];
-		dishes[0] = spaghetti;
-		dishes[1] = alfredo;
-		dishes[2] = friedrice;
-		dishes[3] = cheesesandwich;
-		dishes[4] = omelette;
-
-		printStringArray(dishes);
-
+		String[][] dishes = {
+			{ "Alfredo", "",
+				"https://www.modernhoney.com/fettuccine-alfredo/",
+				"Pasta noodles", "Butter", "Heavy Cream", "Garlic",
+				"Parmesan Cheese", "Black Pepper" },
+		{
+				"Spaghetti",
+				"",
+				"https://www.foodnetwork.com/recipes/food-network-kitchen/simple-spaghetti-with-tomato-sauce-3362665",
+				"Pasta noodles", "Tomatoes", "Tomato sauce", "Cheese",
+				"Garlic", "Basil", "Onion", "Ground Beef",
+				"Black pepper", "Olive oil", "Salt" },
+		{
+				"Fried Rice",
+				"",
+				"https://www.allrecipes.com/recipe/79543/fried-rice-restaurant-style/",
+				"Eggs", "Garlic", "Rice", "Scallion", "Onion",
+				"Carrot", "Soy sauce", "Peas", "Sesame oil",
+				"Vegetable Oil" },
+		{
+				"Grilled Cheese Sandwich",
+				"",
+				"https://www.allrecipes.com/recipe/23891/grilled-cheese-sandwich/",
+				"Cheese", "Bread", "Butter" },
+		{
+				"Omelette",
+				"",
+				"https://www.delish.com/cooking/recipe-ideas/a24892843/how-to-make-omelet/",
+				"Cheese", "Butter", "Black Pepper", "Ham", "Milk",
+				"Eggs", "Salt" },
+		{
+				"Meatloaf",
+				"",
+				"https://www.thewholesomedish.com/the-best-classic-meatloaf/",
+				"Ground Beef", "Eggs", "Bread Crumbs", "Onion",
+				"Ketchup", "Brown Sugar", "Milk", "Mustard", "Salt",
+				"Pepper", "Parsley", "Garlic", "Red Wine Vinegar",
+				"Black pepper" },
+		{
+				"Macaroni and Cheese",
+				"",
+				"https://www.allrecipes.com/recipe/11679/homemade-mac-and-cheese/",
+				"Cheese", "Macaroni Pasta", "Milk", "Butter",
+				"Bread Crumbs", "All-purpose flour", "Paprika"}
+				};
+		
+		String[] keywords = {"Eggs", "Cheese", "Garlic"};
+		
+		for (int i = 0; i < dishes.length; i++) {
+			int per = percentage(dishes[i], keywords);
+			dishes[i][1] = String.valueOf(per);
+		}
+		
 		bubbleSortStringArray(dishes);
-
-		System.out.println("after sorting !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		
+		System.out.println("after sorting !!!!!!!!!!!!!!!!");
+		
 		printStringArray(dishes);
-
+		
+		
 	}
 
 	private static void printStringArray(String[][] twoDeArray) {
-		for (int i = 0; i < twoDeArray.length; i++) {
-			for (int j = 0; j < twoDeArray[i].length; j++) {
-				System.out.print(twoDeArray[i][j] + ",    ");
-			}
-			System.out.println("");
-		}
+		// code to come
+	}
+	
+	private static int percentage(String[] dish, String[] keywords) {
+		// code to come
 	}
 
 	private static void bubbleSortStringArray(String[][] twoDeArray) {
-		int leng = twoDeArray.length;
-		for (int i = 0; i < leng - 1; i++) {
-			for (int j = 0; j < leng - i - 1; j++) {
-				float x = Float.parseFloat(twoDeArray[j][1]);
-				float y = Float.parseFloat(twoDeArray[j+1][1]);
-				if (x > y) {
-					String[] temp = twoDeArray[j];
-					twoDeArray[j] = twoDeArray[j+1];
-					twoDeArray[j+1] = temp;
-				}
-			}
-		}
+		// code to come
 	}
-
 }
