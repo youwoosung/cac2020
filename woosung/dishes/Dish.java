385eabf637e72eb7808b7337be3951ce55f5cdea
youwoosung <67929857+youwoosung@users.noreply.github.com>
2020-11-01 09:30:05 -0500

    Update Dish.java

--- woosung/dishes/Dish.java
+++ woosung/dishes/Dish.java
@@ -1,14 +1,12 @@
+package dishes;
 
 public class Dish {
 	String name;
-	int percentage;
+	float percentage;
 	String link;
 	String[] ingredients;
-	
-	public Dish(String namePassedIn, 
-			int percentagePassedIn, 
-			String linkPassedIn, 
-			String[] ingredientsPassedIn) {
+
+	public Dish(String namePassedIn, int percentagePassedIn, String linkPassedIn, String[] ingredientsPassedIn) {
 		this.name = namePassedIn;
 		this.percentage = percentagePassedIn;
 		this.link = linkPassedIn;
@@ -16,10 +14,34 @@ public class Dish {
 	}
 
 	public void displayAllInfo() {
-    	//to be completed
-  }
+		System.out.println("Name: " + this.name);
+		System.out.println("Percentage: " + this.percentage);
+		System.out.println("Link: " + this.link);
+		System.out.print("Ingredients: ");
+		for (int i = 1; i < ingredients.length; i++) {
+			System.out.print(ingredients[i] + ", ");
+		}
+
+		System.out.println("");
+		System.out.println("");
+	}
 	
+	public void displayNameAndPercentage() {
+		//to be completed
+	}
+
 	public void caculatePercentage(String[] keywords) {
-		  //to be completed
+		int count = 0;
+		float percentage = 0;
+		for (int i = 0; i < keywords.length; i++) {
+			for (int a = 0; a < ingredients.length; a++) {
+				if (keywords[i] == ingredients[a]) {
+					count = count + 1;
+				}
+			}
+		}
+
+		percentage = ((float) count / ((float) ingredients.length)) * 100;
+		this.percentage = (float) ((int) (percentage * 100)) / 100;
 	}
 }
